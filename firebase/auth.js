520305c17b6d2cddf6a632ea1793493f00653fae
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-17 04:48:41 +0100

    Update auth.js

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,1 +1,59 @@
+import { auth }
 
+from "./firebase-config.js";
+
+import {
+
+signInWithEmailAndPassword
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
+
+export async function loginAdmin(
+
+email,
+
+password
+
+){
+
+try{
+
+const userCredential =
+
+await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+return {
+
+success:true,
+
+user:userCredential.user
+
+};
+
+}
+
+catch(error){
+
+return{
+
+success:false,
+
+message:error.message
+
+};
+
+}
+
+}
