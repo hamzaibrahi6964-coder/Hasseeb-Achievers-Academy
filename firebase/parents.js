73b4f59c197dfb286885725999ac105ce80c1165
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-17 14:23:43 +0100

    Update parents.js

--- firebase/parents.js
+++ firebase/parents.js
@@ -1,1 +1,63 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addParent(data){
+
+try{
+
+await addDoc(
+
+collection(db,"parents"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
+
+export async function getParents(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"parents")
+
+);
+
+return snapshot.docs.map(doc => ({
+
+id: doc.id,
+
+...doc.data()
+
+}));
+
+}
