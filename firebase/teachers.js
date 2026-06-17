bde5d6dd775d2b24aa04d7c3f9c0cd94c18272d4
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-17 13:34:05 +0100

    Update teachers.js

--- firebase/teachers.js
+++ firebase/teachers.js
@@ -1,1 +1,63 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addTeacher(data){
+
+try{
+
+await addDoc(
+
+collection(db,"teachers"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
+
+export async function getTeachers(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"teachers")
+
+);
+
+return snapshot.docs.map(doc => ({
+
+id: doc.id,
+
+...doc.data()
+
+}));
+
+}
