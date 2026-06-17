eab3e0bd6047fdc7133b6f461111334b8aff8a3f
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-17 06:09:29 +0100

    Update students.js

--- firebase/students.js
+++ firebase/students.js
@@ -1,1 +1,41 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addStudent(data){
+
+try{
+
+await addDoc(
+
+collection(db,"students"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
