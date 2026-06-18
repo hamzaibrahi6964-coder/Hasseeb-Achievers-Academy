34b356034564942bb2b9affa55077f605262d776
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-18 12:57:33 +0100

    Update questions.js

--- firebase/questions.js
+++ firebase/questions.js
@@ -1,1 +1,63 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addQuestion(data){
+
+try{
+
+await addDoc(
+
+collection(db,"questions"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
+
+export async function getQuestions(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"questions")
+
+);
+
+return snapshot.docs.map(doc => ({
+
+id: doc.id,
+
+...doc.data()
+
+}));
+
+}
