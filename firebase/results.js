344c1796fb9663d2976d20feb269b97270bf3eb3
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-18 08:18:49 +0100

    Add functions to add and retrieve results from Firestore

--- firebase/results.js
+++ firebase/results.js
@@ -1,1 +1,63 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addResult(data){
+
+try{
+
+await addDoc(
+
+collection(db,"results"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
+
+export async function getResults(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"results")
+
+);
+
+return snapshot.docs.map(doc => ({
+
+id: doc.id,
+
+...doc.data()
+
+}));
+
+}
