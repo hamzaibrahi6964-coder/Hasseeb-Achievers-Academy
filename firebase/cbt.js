e71c4b36b03784cd5d2ff4fc23422b1c2619b153
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-18 13:47:17 +0100

    Implement addExam and getExams functions for Firestore

--- firebase/cbt.js
+++ firebase/cbt.js
@@ -1,1 +1,63 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addExam(data){
+
+try{
+
+await addDoc(
+
+collection(db,"cbt"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
+
+export async function getExams(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"cbt")
+
+);
+
+return snapshot.docs.map(doc => ({
+
+id: doc.id,
+
+...doc.data()
+
+}));
+
+}
