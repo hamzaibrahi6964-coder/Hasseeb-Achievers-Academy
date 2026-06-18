3e72dbe6438662367f654899f804a90b99d5e3e0
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-18 07:45:29 +0100

    Implement attendance functions for Firestore

--- firebase/attendance.js
+++ firebase/attendance.js
@@ -1,1 +1,63 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+addDoc,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function addAttendance(data){
+
+try{
+
+await addDoc(
+
+collection(db,"attendance"),
+
+data
+
+);
+
+return true;
+
+}
+
+catch(error){
+
+console.error(error);
+
+return false;
+
+}
+
+}
+
+export async function getAttendance(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"attendance")
+
+);
+
+return snapshot.docs.map(doc => ({
+
+id: doc.id,
+
+...doc.data()
+
+}));
+
+}
