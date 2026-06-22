571af8a46b767ebe18a72788cad04116ac6cd508
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-22 03:40:22 +0100

    Update assignments.js

--- firebase/assignments.js
+++ firebase/assignments.js
@@ -1,1 +1,35 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+collection,
+
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
+
+export async function getAssignments(){
+
+const snapshot =
+
+await getDocs(
+
+collection(db,"assignments")
+
+);
+
+return snapshot.docs.map(doc=>({
+
+id:doc.id,
+
+...doc.data()
+
+}));
+
+}
